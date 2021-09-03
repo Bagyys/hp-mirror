64941177fb758dcca8108dff987f2295ded6a747
ivertadev <iverta.dev@iverta.eu>
2021-09-03 08:14:29 +0300

    Minor fixes because of naming convention

--- client/src/components/FlatReviewContentent/FlatInfo/flatInfo.tsx
+++ client/src/components/FlatReviewContentent/FlatInfo/flatInfo.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //Style
-import classes from './flatInfo.module.scss';
+import classes from "./flatInfo.module.scss";
 //Component
-import AppartmentsRewie from './AppartmentsReview/AppartmentsReview';
+import ApartmentsReview from "./ApartmentsReview/ApartmentsReview";
 
 const FlatInfo = () => {
   return (
     <>
       <div className={classes.Layer}>
-        <AppartmentsRewie />
+        <ApartmentsReview />
         <h1>About the place</h1>
         <h1>Whats this place offers</h1>
         <form>Payment and reservation info</form>
